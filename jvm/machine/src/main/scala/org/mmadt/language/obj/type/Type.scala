35680807dd515f37b1c3b2eb184808bc3b660959
Marko A. Rodriguez <okrammarko@gmail.com>
2020-02-17 18:50:22 -0700

    fixed the a bug in the <= range specifier. also, this.type cleaning up the show a bit here as well.

--- jvm/machine/src/main/scala/org/mmadt/language/obj/type/Type.scala
+++ jvm/machine/src/main/scala/org/mmadt/language/obj/type/Type.scala
@@ -23,9 +23,9 @@
 package org.mmadt.language.obj.`type`
 
 import org.mmadt.language.obj.op._
-import org.mmadt.language.obj.value.{RecValue, StrValue, Value}
-import org.mmadt.language.obj.{Bool, Inst, Obj, Str, TQ}
-import org.mmadt.language.{Tokens, obj}
+import org.mmadt.language.obj.value.{RecValue,StrValue,Value}
+import org.mmadt.language.obj.{Bool,Inst,Obj,Str,TQ}
+import org.mmadt.language.{Tokens,obj}
 import org.mmadt.processor.obj.`type`.CompilingProcessor
 import org.mmadt.processor.obj.`type`.util.InstUtil
 
@@ -35,80 +35,80 @@ import org.mmadt.processor.obj.`type`.util.InstUtil
 trait Type[T <: Type[T]] extends Obj
   with ModelOp {
 
-  def canonical(): this.type = this.range().q(1) //
-  def range(): this.type //
+  def canonical():this.type = this.range().q(1) //
+  def range():this.type //
 
-  def domain[TT <: Type[_]](): TT = (this.insts() match {
+  def domain[D <: Type[_]]():D = (this.insts() match {
     case Nil => this
-    case i: List[(Type[_], Inst)] => i.head._1.range()
-  }).asInstanceOf[TT]
+    case i:List[(Type[_],Inst)] => i.head._1.range()
+  }).asInstanceOf[D]
 
-  def insts(): List[(Type[_], Inst)] //
+  def insts():List[(Type[_],Inst)] //
 
-  def linvert(): this.type = {
+  def linvert():this.type ={
     ((this.insts().tail match {
       case Nil => this.range()
-      case i => i.foldLeft[Obj](i.head._1.range())((btype, inst) => inst._2.apply(btype, inst._2.args()))
+      case i => i.foldLeft[Obj](i.head._1.range())((btype,inst) => inst._2.apply(btype,inst._2.args()))
     }) match {
-      case vv: Value[_] => vv.start()
+      case vv:Value[_] => vv.start()
       case x => x
     }).asInstanceOf[this.type]
   }
 
-  def rinvert[TT <: Type[_]](): TT =
+  def rinvert[TT <: Type[_]]():TT =
     (this.insts().dropRight(1).lastOption match {
-      case Some(prev) => prev._2.apply(prev._1, prev._2.args())
+      case Some(prev) => prev._2.apply(prev._1,prev._2.args())
       case None => this.insts().head._1
     }).asInstanceOf[TT]
 
-  def compose[TT <: Type[_]](btype: TT): TT = {
-    var a: this.type = this
-    for (i <- btype.insts()) a = a.compose(i._1, i._2)
+  def compose[TT <: Type[_]](btype:TT):TT ={
+    var a:this.type = this
+    for (i <- btype.insts()) a = a.compose(i._1,i._2)
     a.asInstanceOf[TT]
   }
 
-  def compose(inst: Inst): this.type //
-  def compose[TT <: Type[_]](t2: Obj, inst: Inst): TT = (t2 match {
-    case _: Bool => bool(inst)
-    case _: obj.Int => int(inst)
-    case _: Str => str(inst)
-    case _: RecType[Obj, Obj] => rec(t2.asInstanceOf[RecType[Obj, Obj]], inst)
+  def compose(inst:Inst):this.type //
+  def compose[TT <: Type[_]](t2:Obj,inst:Inst):TT = (t2 match {
+    case _:Bool => bool(inst)
+    case _:obj.Int => int(inst)
+    case _:Str => str(inst)
+    case _:RecType[Obj,Obj] => rec(t2.asInstanceOf[RecType[Obj,Obj]],inst)
   }).asInstanceOf[TT]
 
-  def int(inst: Inst, q: TQ = this.q()): IntType //
-  def bool(inst: Inst, q: TQ = this.q()): BoolType //
-  def str(inst: Inst, q: TQ = this.q()): StrType //
-  def rec[A <: Obj, B <: Obj](tvalue: RecType[A, B], inst: Inst, q: TQ = this.q()): RecType[A, B] //
+  def int(inst:Inst,q:TQ = this.q()):IntType //
+  def bool(inst:Inst,q:TQ = this.q()):BoolType //
+  def str(inst:Inst,q:TQ = this.q()):StrType //
+  def rec[A <: Obj,B <: Obj](tvalue:RecType[A,B],inst:Inst,q:TQ = this.q()):RecType[A,B] //
 
-  final def <=[TT <: Type[TT]](mapFrom: TT with Type[TT]): TT = mapFrom.q(this.q()) //
-  def ==>[TT <: Type[TT]](t: TT with Type[TT]): TT = new CompilingProcessor().apply(this, t).next().obj()
+  final def <=[D <: Type[D]](domain:D with Type[D]):this.type = domain.compose(this).q(this.q()).asInstanceOf[this.type] //
+  def ==>[TT <: Type[TT]](atype:TT with Type[TT]):TT = new CompilingProcessor().apply(this,atype).next().obj()
 
-  override def id(): this.type = this.compose(IdOp()).asInstanceOf[this.type] //
-  override def map[O <: Obj](other: O): O = this.compose(other, MapOp(other)) //
-  override def model(model: StrValue): this.type = this.compose(ModelOp(model)).asInstanceOf[this.type] //
-  override def from[O <: Obj](label: StrValue): O = this.compose(FromOp(label)).asInstanceOf[O] //
-  override def as[O <: Obj](name: String): O = (InstUtil.lastInst(this) match {
+  override def id():this.type = this.compose(IdOp()) //
+  override def map[O <: Obj](other:O):O = this.compose(other,MapOp(other)) //
+  override def model(model:StrValue):this.type = this.compose(ModelOp(model)) //
+  override def from[O <: Obj](label:StrValue):O = this.compose(FromOp(label)).asInstanceOf[O] //
+  override def as[O <: Obj](name:String):O = (InstUtil.lastInst(this) match {
     case Some(x) if x == AsOp(name) => this
     case _ => this.compose(AsOp(name))
   }).asInstanceOf[O] //
   // pattern matching methods
-  override def test(other: Obj): Boolean = other match {
-    case argValue: Value[_] => TypeChecker.matchesTV(this, argValue)
-    case argType: Type[_] => TypeChecker.matchesTT(this, argType)
+  override def test(other:Obj):Boolean = other match {
+    case argValue:Value[_] => TypeChecker.matchesTV(this,argValue)
+    case argType:Type[_] => TypeChecker.matchesTT(this,argType)
   }
 
-  override def equals(other: Any): Boolean = other match {
-    case t: Type[T] => t.insts().map(_._2) == this.insts().map(_._2) && this.range().toString == t.range().toString
+  override def equals(other:Any):Boolean = other match {
+    case t:Type[T] => t.insts().map(_._2) == this.insts().map(_._2) && this.range().toString == t.range().toString
     case _ => false
   }
 
   // standard Java implementations
-  override def hashCode(): scala.Int = this.range().toString.hashCode // TODO: using toString()
+  override def hashCode():scala.Int = this.range().toString.hashCode // TODO: using toString()
 
-  def |[O <: Type[O]](other: O): O = { // TODO: ghetto union type construction
+  def |[O <: Type[O]](other:O):O ={ // TODO: ghetto union type construction
     if (this.insts().nonEmpty && this.insts().head._2.op().equals(Tokens.choose))
-      this.linvert().choose(Map(other -> other) ++ this.insts().head._2.arg[RecValue[O, O]]().value())
+      this.linvert().choose(Map(other -> other) ++ this.insts().head._2.arg[RecValue[O,O]]().value())
     else
-      this.choose(other -> other, this.asInstanceOf[O] -> this.asInstanceOf[O])
+      this.choose(other -> other,this.asInstanceOf[O] -> this.asInstanceOf[O])
   }
 }
