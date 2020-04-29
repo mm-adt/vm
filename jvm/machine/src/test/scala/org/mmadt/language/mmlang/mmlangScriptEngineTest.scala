6e6b7805a4d0a8ba399a2786a61dfa8fb9f61566
Marko Rodriguez <okrammarko@gmail.com>
2020-04-29 14:20:00 -0600

    lots of good work on the introduction languages documentation. also, some CSS infrastructure started via docinfo.html.

--- jvm/machine/src/test/scala/org/mmadt/language/mmlang/mmlangScriptEngineTest.scala
+++ jvm/machine/src/test/scala/org/mmadt/language/mmlang/mmlangScriptEngineTest.scala
@@ -156,7 +156,7 @@ class mmlangScriptEngineTest extends FunSuite {
 
   test("refinement type parsing") {
     assertResult(int.q(?) <= int.is(int.gt(int(10))))(engine.eval("int[is,int[gt,10]]"))
-    //assertResult(int <= int.is(int.gt(int(10))))(engine.eval("int<=int[is,int[gt,10]]")) //TODO: when a range is specified by the user, use that during compilation
+    assertResult(int <= int.is(int.gt(int(10))))(engine.eval("int<=int[is,int[gt,10]]")) //TODO: when a range is specified by the user, use that during compilation
     assertResult(int.q(?) <= int.is(int.gt(int(10))))(engine.eval("int[is>10]"))
   }
 
