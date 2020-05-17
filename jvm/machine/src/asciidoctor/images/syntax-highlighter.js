d6103c78ad750d61844560604d0b9df9e7a6f505
Marko Rodriguez <okrammarko@gmail.com>
2020-05-17 11:49:27 -0600

    pulled out the syntax highlighter into it's own JavaScript file so we get syntax highlighting in IntelliJ. refactor of highlighter code.

---
+++ jvm/machine/src/asciidoctor/images/syntax-highlighter.js
@@ -0,0 +1,40 @@
+/**
+ * mmlang Syntax Highlighter that tweaks the CodeRay Python highlighter at HTML load time.
+ * Currently uses JQuery to alter the HTML DOM accordingly.
+ * This file is loaded from docinfo.html <script src="syntax-highlighter.js"/>
+ **/
+$(document).ready(function(){
+  var token = "XXX"
+  var ospan = "<span style='color:"
+  var cspan = "'>" + token + "</span>"
+  var bcolor = "background-color:hsla(0,100%,100%,0.05)"
+  var bold = ";font-weight:bold"
+  // BEGIN: general random tweaks to convert python highlighting to mmlang
+  $("code[data-lang='python']").attr("data-lang", "mmlang")
+  $("span[style='color:#010']").attr("style","color:#710")
+  $("span[style='background-color:hsla(0,100%,50%,0.05)']").attr("style",bcolor)
+  $("span[style='color:#F00;background-color:#FAA']").attr("style","color:#710;" + bcolor)
+  // END: general random tweaks to convert python highlighting to mmlang
+  var rewrites = {"op":      ["[",  "["+ospan + "#080" +bold + cspan],      // bold green
+                  "infix":   ["",       ospan + "#080" +bold + cspan],      // bold green
+                  "vars":    ["[",  "["+ospan + "#080" +bold + cspan],      // bold green
+                  "value":   ["",       ospan + "#00D" + cspan],            // light blue
+                  "type":    ["",       ospan + "#369" + bold + cspan],     // dark blue
+                  "special": ["",       ospan + "#710" + cspan],            // dark red
+                  "plain":   ["",       ospan + "#000" + cspan]};           // normal black text
+  var items =    {"op":      ["a","branch","neg","get","gte","lte","gt","lt","mult","plus","start",
+                              "one","zero","noop","eq","is","id","db","explain","count","fold","map"],
+                  "infix":   ["|"],
+                  "vars":    ["a","b","c"],
+                  "value":   ["true","false"],
+                  "type":    ["_","real","rec","lst","poly","inst","obj","int","str","bool","range","domain"],
+                  "special": ["mmlang","q"],
+                  "plain":   ["is not"]};
+  $.each(items, function(key, values) {
+    jQuery.each(values, function(index,element) {
+      $("code:contains('"+$.escapeSelector(rewrites[key][0]+element)+"')").html(function(_, html) {
+        return html.replace(new RegExp($.escapeSelector(rewrites[key][0]+element),"gi"), rewrites[key][1].replace(new RegExp(token,"gi"),element));
+      });
+     });
+   })
+});
