65cbc4959997f1fbadf241a9d3df5b4237aac036
Marko Rodriguez <okrammarko@gmail.com>
2020-05-20 17:14:47 -0600

    updated site

--- images/syntax-highlighter.js
+++ images/syntax-highlighter.js
@@ -4,7 +4,7 @@
  * This file is loaded from docinfo.html <script src="syntax-highlighter.js"/>
  **/
 $(document).ready(function(){
-  $("a[href^='https://en.wikipedia.org/wiki']").wrap("<span id='p2'></span>") // WIKIPEDIA PAGE PREVIEW
+  $("a[href^='https://en.wikipedia.org/wiki']").miniPreview({prefetch:'parenthover',width:800,height:400,scale:0.9}) // WIKIPEDIA PAGE PREVIEW
   //////////////////
   var token = "XXX"
   var ospan = "<span style='color:"
