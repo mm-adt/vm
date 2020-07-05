a23e995320dc9391db9256806ad09556d404ddde
Marko Rodriguez <okrammarko@gmail.com>
2020-07-05 03:20:47 -0600

    tp3<=kv functor for edge mappings.

--- jvm/machine/src/main/resources/model/functor/tp3_kv.mm
+++ jvm/machine/src/main/resources/model/functor/tp3_kv.mm
@@ -1,3 +1,9 @@
 // A Functor from Key/Value model-ADT to TP3 model-ADT
-[define,vertex<=kv:('k' -> (['vertex'|'edge'],obj),'v'     -> (str->obj))-<
-                   ('id'-> .k.1,                   'label' -> [.v.label|.k.0])]
+[define,vertex<=kv:('k'->(is=='vertex',obj),'v'->(str->obj))-<
+                   ('id'    -> .k.1,
+                    'label' -> [.v.label|.k.0]),
+          edge<=kv:('k'->(is=='edge',obj),'v'->('link'->(obj,obj),str->obj))-<
+                   ('id'    -> .k.1,
+                    'label' -> [.v.label|.k.0],
+                    'outV'  -> .v.link.0-<('vertex',_),
+                    'inV'   -> .v.link.1-<('vertex',_))]
