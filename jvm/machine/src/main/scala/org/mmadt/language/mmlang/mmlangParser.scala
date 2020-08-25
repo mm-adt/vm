a153b3714bf34714e573aff6d8c2d06699628fa2
Marko Rodriguez <okrammarko@gmail.com>
2020-08-25 11:42:27 -0600

    documentation processed and generated

--- jvm/machine/src/main/scala/org/mmadt/language/mmlang/mmlangParser.scala
+++ jvm/machine/src/main/scala/org/mmadt/language/mmlang/mmlangParser.scala
@@ -91,19 +91,19 @@ class mmlangParser extends JavaTokenParsers {
   lazy val objName: Parser[String] = symbolName <~ ":"
 
   // poly parsing
-  lazy val polySep: Parser[String] = Tokens.| | Tokens.`;` | Tokens.`,`
+  lazy val polySep: Parser[String] = Tokens.| | Tokens.`;` | Tokens.`,` | Tokens.juxt_op
   lazy val lstObj: Parser[Lst[Obj]] = lstValue | lstType
   lazy val recObj: Parser[Rec[Obj, Obj]] = recValue | recType
   lazy val polyObj: Parser[Poly[Obj]] = lstObj | recObj
 
   def lstStruct(parser: Parser[Obj]): Parser[LstTuple[Obj]] =
-    (opt(parser) ~ polySep) ~ repsep(opt(parser), polySep) ^^ (x => lst(x._1._2, x._1._1.getOrElse(zeroObj) +: x._2.map(y => y.getOrElse(zeroObj)): _*).g) |
+    (opt(parser) ~ polySep) ~ repsep(opt(parser), polySep) ^^ (x => (Some(x._1._2).map(y => if(y == juxt_op) Tokens.`;` else y).get, x._1._1.getOrElse(zeroObj) +: x._2.map(y => y.getOrElse(zeroObj)))) |
       parser ^^ (x => (Tokens.`,`, List(x))) |
       Tokens.empty ^^ (_ => (Tokens.`,`, List.empty[Obj]))
 
   def recStruct(parser: Parser[Obj]): Parser[RecTuple[Obj, Obj]] =
     ((opt((parser <~ Tokens.->) ~ parser) ~ polySep) ~ rep1sep(opt((parser <~ Tokens.->) ~ parser), polySep)) ^^
-      (x => rec(g = (x._1._2, x._1._1.map(a => List(a._1 -> a._2)).getOrElse(List.empty) ++ x._2.map(y => y.map(z => z._1 -> z._2).getOrElse(zeroObj -> zeroObj)).toMap[Obj, Obj])).g) |
+      (x => (Some(x._1._2).map(y => if(y == juxt_op) Tokens.`;` else y).get, x._1._1.map(a => List(a._1 -> a._2)).getOrElse(List.empty) ++ x._2.map(y => y.map(z => z._1 -> z._2).getOrElse(zeroObj -> zeroObj)).toMap[Obj, Obj])) |
       Tokens.-> ^^ (_ => (Tokens.`,`, List.empty)) |
       (parser <~ Tokens.->) ~ parser ^^ (x => (Tokens.`,`, List(x._1 -> x._2)))
 
