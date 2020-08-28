c9149efe8fec9057355dd4d77363262cde947b27
Marko Rodriguez <okrammarko@gmail.com>
2020-08-28 00:21:33 -0600

    started working on the algebra section of the documentation again. the obj magma is the obj monoid now and have tightened up the obj monoid and inst monoid sections. diagram conversion to SVG.

--- jvm/machine/src/main/scala/org/mmadt/language/obj/op/branch/CombineOp.scala
+++ jvm/machine/src/main/scala/org/mmadt/language/obj/op/branch/CombineOp.scala
@@ -26,7 +26,6 @@ import org.mmadt.language.Tokens
 import org.mmadt.language.obj.Inst.Func
 import org.mmadt.language.obj._
 import org.mmadt.language.obj.op.{BranchInstruction, TraceInstruction}
-import org.mmadt.language.obj.value.strm.Strm
 import org.mmadt.storage.obj.value.VInst
 
 trait CombineOp[+A <: Obj] {
