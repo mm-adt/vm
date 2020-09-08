253951d60571a508d819576388235b578b7152a3
Marko Rodriguez <okrammarko@gmail.com>
2020-09-08 15:19:06 -0600

    converted the examples from last night into the @spmallette test suite format. boring tedious work.

--- jvm/machine/src/test/scala/org/mmadt/language/model/digraphTest.scala
+++ jvm/machine/src/test/scala/org/mmadt/language/model/digraphTest.scala
@@ -25,11 +25,10 @@ package org.mmadt.language.model
 import org.mmadt.language.LanguageException
 import org.mmadt.language.model.digraphTest.DIGRAPH
 import org.mmadt.language.obj.Obj.{intToInt, stringToStr, symbolToToken, tupleToRecYES}
-import org.mmadt.language.obj.`type`.__
 import org.mmadt.language.obj.`type`.__._
+import org.mmadt.language.obj.`type`.{Type, __}
 import org.mmadt.language.obj.op.trace.ModelOp.Model
 import org.mmadt.processor.inst.BaseInstTest
-import org.mmadt.processor.inst.BaseInstTest.engine
 import org.mmadt.processor.inst.TestSetUtil.{comment, excepting, testSet, testing}
 import org.mmadt.storage
 import org.mmadt.storage.StorageFactory.{str, _}
@@ -52,15 +51,25 @@ class digraphTest extends BaseInstTest(
         'attr(str("key") -> str("age") `_,` str("value") -> int(29))),
       "[('name';'marko'),('age';29)] => [map,id][as,attr]"),*/
     excepting((20 `;` "marko"), 'attr, LanguageException.typingError((20 `;` "marko"), 'attr), "(20;'marko') => attr"),
-  )) {
-  test("mm-ADT int") {
-   /* println(engine.eval(s":[model,${storage.model("digraph")}]"))
-    //println(engine.eval("[map,digraph]"))
-    println(engine.eval("(32;('name';'marko')) => [as,(int;attr)]"))
-    println(engine.eval("(32;('name';'marko')) => (int;attr) => vertex"))
-    println(engine.eval("(32;('name';'marko')) => vertex"))
-    println(engine.eval("23 => vertex"))
-    println(engine.eval("-23 => vertex"))
-    engine.eval(":{1}")*/
-  }
-}
+    comment("vertex via int"),
+    testing(23, 'vertex, 'vertex(str("id") -> 'nat(23) `,`), "23 => vertex"),
+    testing('nat(23), 'vertex, 'vertex(str("id") -> 'nat(23) `,`), "nat:23 => vertex"),
+    testing(-23, 'vertex, 'vertex(str("id") -> 'nat(23) `_,` str("attrs") -> 'attr(str("key") -> str("no") `_,` str("value") -> str("data"))), "-23 => vertex"),
+    excepting(0, 'vertex, LanguageException.typingError((0 `;`("no" `;` "data")).q(qZero), (str("id") -> __("nat") `_,` str("attrs") -> 'attr.q(*)).asInstanceOf[Type[_]]), "0 => vertex"),
+    comment("vertex via int/pair"),
+    testing(
+      (32 `;`("name" `;` "marko")),
+      as(int `;` 'attr),
+      (32 `;` 'attr(str("key") -> str("name") `_,` str("value") -> str("marko"))),
+      "(32;('name';'marko')) => [as,(int;attr)]"), // shouldn't need [as]
+    testing(
+      (32 `;`("name" `;` "marko")),
+      (int `;` 'attr) `=>` 'vertex,
+      'vertex(str("id") -> 'nat(32) `_,` str("attrs") -> 'attr(str("key") -> str("name") `_,` str("value") -> str("marko"))),
+      "(32;('name';'marko')) => (int;attr) => vertex"),
+    testing(
+      (32 `;`("name" `;` "marko")),
+      'vertex,
+      'vertex(str("id") -> 'nat(32) `_,` str("attrs") -> 'attr(str("key") -> str("name") `_,` str("value") -> str("marko"))),
+      "(32;('name';'marko')) => vertex"),
+  ))
