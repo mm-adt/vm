2cf32a67b009ae8c7c989c7f932e2477941f88ea
Marko Rodriguez <okrammarko@gmail.com>
2020-09-21 15:16:17 -0600

    forgot to updated mmkv tests with strm name work.

--- jvm/machine/src/test/scala/org/mmadt/storage/mmkv/mmkvInstTest.scala
+++ jvm/machine/src/test/scala/org/mmadt/storage/mmkv/mmkvInstTest.scala
@@ -52,10 +52,10 @@ class mmkvInstTest extends FunSuite {
 
   test("mmkv parsing") {
     assertResult("mmkv{*}")(engine.eval(s"[=mmkv,'${file1}']").range.toString)
-    assertResult("[mmkv:('k'->1,'v'->'marko'),mmkv:('k'->2,'v'->'ryan'),mmkv:('k'->3,'v'->'stephen'),mmkv:('k'->4,'v'->'kuppitz')]")(engine.eval(s"1[=mmkv,'${file1}']").toString)
+    assertResult("mmkv:[mmkv:('k'->1,'v'->'marko'),mmkv:('k'->2,'v'->'ryan'),mmkv:('k'->3,'v'->'stephen'),mmkv:('k'->4,'v'->'kuppitz')]")(engine.eval(s"1[=mmkv,'${file1}']").toString)
     //
     assertResult("mmkv{*}")(engine.eval(s"[=mmkv,'${file2}']").range.toString)
-    assertResult("[" +
+    assertResult("mmkv:[" +
       "mmkv:('k'->1,'v'->person:('name'->'marko','age'->29))," +
       "mmkv:('k'->2,'v'->person:('name'->'ryan','age'->25))," +
       "mmkv:('k'->3,'v'->person:('name'->'stephen','age'->32))," +
@@ -84,7 +84,7 @@ class mmkvInstTest extends FunSuite {
 
   test("[=mmkv] with mmkv-1.mm") {
     assertResult(s"mmkv{*}<=_[=mmkv,'${file1}']")(__.=:(mmkv)(str(file1)).toString)
-    assertResult("[mmkv:('k'->1,'v'->'marko'),mmkv:('k'->2,'v'->'ryan'),mmkv:('k'->3,'v'->'stephen'),mmkv:('k'->4,'v'->'kuppitz')]")(int(1).=:(mmkv)(str(file1)).toString)
+    assertResult("mmkv:[mmkv:('k'->1,'v'->'marko'),mmkv:('k'->2,'v'->'ryan'),mmkv:('k'->3,'v'->'stephen'),mmkv:('k'->4,'v'->'kuppitz')]")(int(1).=:(mmkv)(str(file1)).toString)
     assertResult(int(1, 2, 3, 4))(Processor.iterator(int(4), int.=:[Rec[Obj, Obj]](mmkv)(str(file1)).get(str("k"), int)))
     // assertResult("mmkv:('k'->1,'v'->'marko')")((int(1) ==> int.=:[RecType[Obj, Obj]](mmkv)(str(file1))).toStrm.values.iterator.next().toString)
   }
